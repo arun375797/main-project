b30dfab54372669f92314a397e8b4ebfb5c7817c
elizabethkjacob <elizabethkjacob2000@gmail.com>
2024-05-03 21:31:28 +0530

    styling

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -6,8 +6,9 @@ const Chat = () => {
   const [chatHistory, setChatHistory] = useState([]);
   const [commentMessage, setCommentMessage] = useState("");
   const [showCommentField, setShowCommentField] = useState([]);
-  const [editIndex, setEditIndex] = useState(-1); // State variable to hold the index of the message being edited
-  const [editMessage, setEditMessage] = useState(""); // State variable to hold the message content being edited
+  const [editIndex, setEditIndex] = useState(-1); 
+  const [editMessage, setEditMessage] = useState(""); 
+
   useEffect(() => {
     fetchChatHistory();
   }, []);
@@ -51,10 +52,9 @@ const Chat = () => {
       console.error("Error sending comment message:", error);
     }
     setShowCommentField(showCommentField.map((value, i) => (i === index ? false : value)));
-    setCommentMessage(""); // Reset commentMessage state
+    setCommentMessage(""); 
   };
   
-
   const handleCommentButtonClick = (index) => {
     setShowCommentField(showCommentField.map((value, i) => (i === index ? true : value)));
   };
@@ -69,86 +69,110 @@ const Chat = () => {
       console.error("Error deleting message:", error);
     }
   };
-    const handleEditMessage = (index, message) => {
-      setEditIndex(index); // Set the index of the message being edited
-      setEditMessage(message); // Set the current message to the edit message state
-    };
+  
+  const handleEditMessage = (index, message) => {
+    setEditIndex(index); 
+    setEditMessage(message); 
+  };
 
   const handleSaveEdit = async (index) => {
     try {
       await axios.put(`http://localhost:5000/api/student/chat-messages/${chatHistory[index]._id}`, { message: editMessage });
       const updatedChatHistory = [...chatHistory];
-      updatedChatHistory[index].message = editMessage; // Update the message content in the chat history
+      updatedChatHistory[index].message = editMessage; 
       setChatHistory(updatedChatHistory);
-      setEditIndex(-1); // Reset edit index
-      setEditMessage(""); // Reset edit message
+      setEditIndex(-1); 
+      setEditMessage(""); 
     } catch (error) {
       console.error("Error updating message:", error);
     }
   };
 
-  return (
-    <div className="container mt-5">
-    <h3>Chat</h3>
-    <div className="mb-3">
-      <input 
-        type="text" 
-        className="form-control" 
-        placeholder="Type your message" 
-        value={chatMessage} 
-        onChange={handleChatMessageChange} 
-      />
-      <button className="btn btn-primary mt-2" onClick={handleSendChatMessage}>Send Message</button>
-    </div>
-    <ul className="list-group  ">
-      {chatHistory.map((message, index) => (
-        <li key={index} className="list-group-item d-flex  justify-content-between align-items-center">
-          <div>
-            {message.message}
-            <ul className="list-group mt-2 ">
-              {message.comments.map((comment, commentIndex) => (
-                <li key={`${index}_${commentIndex}`} className="list-group-item bg-light">
-                  {comment}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <button className="btn btn-primary me-2" onClick={() => handleCommentButtonClick(index)}>Add Comment</button>
-            {index === editIndex ? (
-              <div className="input-group">
-                <input 
-                  type="text" 
-                  className="form-control" 
-                  value={editMessage.message} // Display message content in the input field
-                  onChange={(event) => setEditMessage(event.target.value)} 
-                />
-                <button className="btn btn-success" onClick={() => handleSaveEdit(index)}>Save</button>
-              </div>
-            ) : (
-              <button className="btn btn-secondary" onClick={() => handleEditMessage(index, message)}>Edit</button>
-            )}
-            <button className="btn btn-danger" onClick={() => handleDeleteMessage(index)}>Delete</button>
-          </div>
-          {showCommentField[index] && (
-            <div className="mt-2">
-              <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Add a comment" 
-                value={commentMessage} 
-                onChange={handleCommentMessageChange} 
-              />
-              <button className="btn btn-primary mt-2" onClick={() => handleSendCommentMessage(index, commentMessage)}>Comment</button>
-            </div>
-          )}
-        </li>
-      ))}
-    </ul>
-  </div>
-  
-  );
-  
-};
+  return(
+    <div style={{ 
 
+      minHeight: '100vh', 
+      padding: '20px' 
+    }}>
+      <div className="container mt-5">
+        <div style={{ border: '2px solid ', borderRadius: '5px', padding: '20px' }}>
+          <h3>Discussion-Form</h3>
+          <br />
+          <div className="mb-3">
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Type your message" 
+              value={chatMessage} 
+              onChange={handleChatMessageChange} 
+            />
+            <button className="btn btn-primary mt-2" onClick={handleSendChatMessage}>📝 Ask Query</button>
+          </div>
+          <ul className="list-group">
+            {chatHistory.map((message, index) => (
+              <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                <div>
+                  {message.message}
+                  <ul className="list-group mt-2">
+                    {message.comments.map((comment, commentIndex) => (
+                      <li key={`${index}_${commentIndex}`} className="list-group-item bg-light">
+                        {comment}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <button className="btn btn-primary me-2" onClick={() => handleCommentButtonClick(index)}>
+                    &#128172; Add Comment
+                  </button>
+                  {index === editIndex ? (
+                    <div className="input-group">
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        value={editMessage.message} 
+                        onChange={(event) => setEditMessage(event.target.value)} 
+                      />
+                      <button className="btn btn-success" onClick={() => handleSaveEdit(index)}>
+                        &#128190; Save
+                      </button>
+                    </div>
+                  ) : (
+                    <button className="btn btn-secondary" onClick={() => handleEditMessage(index, message)}>
+                      &#9998; Edit
+                    </button>
+                  )}
+                  <button className="btn btn-danger" onClick={() => handleDeleteMessage(index)}>
+                    &#128465; Delete
+                  </button>
+                </div>
+                {showCommentField[index] && (
+                  <div className="mt-2">
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      placeholder="Add a comment" 
+                      value={commentMessage} 
+                      onChange={handleCommentMessageChange} 
+                    />
+                    <button className="btn btn-primary mt-2" onClick={() => handleSendCommentMessage(index, commentMessage)}> &#10133; ADD</button>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Chat;
+
+
+
+
+
+
+
+
+//background: `linear-gradient(#001f3f, #0047ab, #007bff)`,
